0763c5761846e7df7fb502ce324a5d6a3c94c2e3
swapnil-bhamat <swapnil.p.bhamat@gmail.com>
2026-05-24 12:53:52 +0530

    chore: remove entire project directory and source code

--- server/api/auth/login.get.ts
+++
@@ -1,4 +0,0 @@
-export default defineEventHandler(async (event) => {
-    const authUrl = getAuthUrl()
-    return sendRedirect(event, authUrl)
-})
--- server/api/auth/user.get.ts
+++
@@ -1,7 +0,0 @@
-export default defineEventHandler(async (event) => {
-    const user = await getGoogleUser(event)
-    if (!user) {
-        return { authenticated: false }
-    }
-    return { authenticated: true, user }
-})
